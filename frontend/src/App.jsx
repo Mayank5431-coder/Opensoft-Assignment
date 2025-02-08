226c4b0c268d5352b58b51405e7b4d7522d416f5
Mayank-coder5431 <mayankpratapsingh679@gmail.com>
2025-02-08 11:09:28 +0530

    Full project Completed Successfully!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import SignupOtp from './auth/SignupOtp';
 import Signup from './auth/Signup';
 import Login from './auth/Login';
 import Dashboard from './auth/Dashboard';
+import NotFound from './auth/Not Found';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path='/signup/otp' element={<SignupOtp />} />
         <Route path='/signin' element={<Login />} />
         <Route path='/Dashboard' element={<Dashboard/>}></Route>
+        <Route path="*" element={<NotFound/>}></Route>
       </Routes>
     </Router>
   );
---
+++ frontend/src/auth/Not Found.jsx
@@ -0,0 +1,9 @@
+export default function NotFound(){
+  return(
+    <div className="flex justify-center items-center bg-black h-screen w-full">
+      <div className="text-white text-4xl font-bold">
+        Error-404!
+      </div>
+    </div>
+  )
+}
